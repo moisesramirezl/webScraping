0e5b5dbb7d8e4363896f8ec7cdb18c1c92683ac4
moisesramirezl <moises.ramirez@latam.com>
2020-05-27 14:28:04 -0400

    scraping from file

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ class Trade(object):
 
 mainTrades = []
 
-url = 'http://larrainvial.finmarketslive.cl/www/index.html?mercado=chile'
-print("visiting... " + url)
-headers = {
-  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
-  'AppleWebKit/537.36 (KHTML, like Gecko)'
-  'Chrome/64.0.3282.167 Safari/537.36'
-}
+# for live scraping use
+# url = 'http://larrainvial.finmarketslive.cl/www/index.html?mercado=chile'
+# print("visiting... " + url)
+# headers = {
+#   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
+#   'AppleWebKit/537.36 (KHTML, like Gecko)'
+#   'Chrome/64.0.3282.167 Safari/537.36'
+# }
         
-result = requests.get(url, headers=headers,verify=True)
+# result = requests.get(url, headers=headers,verify=True)
 
-soup = BeautifulSoup(result.content, 'html.parser')
+soup = BeautifulSoup(open("./page.html"), 'html.parser')
 mainTradesNemoDiv = soup.findAll("span", {"class": "clsConstituyentes"})
 
 itemCount = 0
