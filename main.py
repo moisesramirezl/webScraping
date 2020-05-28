93c095f511b3ef52363b7b88a5d528aa385f2b9b
moisesramirezl <moises.ramirez@latam.com>
2020-05-28 17:00:21 -0400

    add random headers and proxies

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #http://larrainvial.finmarketslive.cl/www/index.html?mercado=chile
 import requests
 from bs4 import BeautifulSoup 
+from getProxies import getRandomProxy
+from randomHeaders import randomHeader
+from logger import Logger
 
 class Trade(object):
     def __init__(self, nemo, lastPrice, dateLastPrice, hourLastPrice):
@@ -11,28 +14,41 @@ class Trade(object):
 
 mainTrades = []
 
-# for live scraping use
-# url = 'http://larrainvial.finmarketslive.cl/www/index.html?mercado=chile'
-# print("visiting... " + url)
-# headers = {
-#   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
-#   'AppleWebKit/537.36 (KHTML, like Gecko)'
-#   'Chrome/64.0.3282.167 Safari/537.36'
-# }
-        
-# result = requests.get(url, headers=headers,verify=True)
 
-soup = BeautifulSoup(open("./page.html"), 'html.parser')
+# for live scraping use
+url = 'http://larrainvial.finmarketslive.cl/www/index.html?mercado=chile'
+      
+
+headers = randomHeader(Logger)
+proxy = getRandomProxy()
+
+print ("visiting: " + url)  
+print ("header: " + str(headers))
+print ("proxy: " + str(proxy))
+
+result = requests.get(url, headers=headers, proxies={"http": proxy, "https": proxy}, verify=True)
+soup = BeautifulSoup(result.content, 'html.parser')
+
 mainTradesNemoDiv = soup.findAll("span", {"class": "clsConstituyentes"})
-mainTradesPriceDiv = soup.findAll("span",{"data-bind":"text: price"})
+mainTradesDiv = soup.findAll("div", {"id":"blkConstituyentes"})
 dateLastPrice = soup.find("span", {"id": "dateUltimaActu"})
 hourLastPrice = soup.find("span", {"id": "timeUltimaActu"})
 
+mainTradesDiv = "<html>" + str(mainTradesDiv) + "</html>"
+soupMainTradesDiv = BeautifulSoup(mainTradesDiv, 'html.parser')
+mainTradesPriceDiv = soupMainTradesDiv.findAll("span", {"data-bind":"text: price"})
+ 
 itemCount = 0
 for trade in mainTradesNemoDiv:
-  mainTrades.append(Trade(trade.contents[0].strip(), mainTradesPriceDiv[itemCount].text.strip(), dateLastPrice.text.strip(), hourLastPrice.text.strip()))
+  mainTrades.append(Trade(trade.contents[0].strip(), 
+    mainTradesPriceDiv[itemCount].text.strip(), 
+    dateLastPrice.text.strip(), 
+    hourLastPrice.text.strip()))
   itemCount+=1
 
-
 for trade in mainTrades:
-    print(trade.nemo + " " + trade.lastPrice + " " + trade.hourLastPrice + " " + trade.dateLastPrice)
+  if(trade.nemo == "LTM"):
+    if(trade.lastPrice < '900'):
+      print ("Compra, precio = " + str(trade.lastPrice))
+    else:
+      print ("Aun no, precio = " + str(trade.lastPrice))
