7d5656253b8451971ee6af591be4988640425beb
moisesramirezl <moises.ramirez@latam.com>
2020-06-01 12:16:36 -0400

    adding local conf to gitignore

--- utils/readConfig.py
+++ utils/readConfig.py
@@ -2,7 +2,7 @@ import json
 tradesAlertRules = {}
 
 def getAlertRules():
-  with open('tradesAlertRules.json', 'r') as inputFile:
+  with open('.userConf/tradesAlertRules.json', 'r') as inputFile:
       tradesAlertRules = inputFile.read()
 
   tradesAlertRulesObj = json.loads(tradesAlertRules)
