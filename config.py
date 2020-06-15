f894574c6cccc37d4b3991fd7234ae7552c9df73
Bladimir Alexander Yanez Pina <bladimir.yanez@latam.com>
2020-06-15 17:06:48 -0400

    Use credentials from Cloud Secret Manager to access to database

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
 import os
+from google.cloud import secretmanager
 
 PROJECT_ID = "1033385642776"
 DATA_BACKEND = "cloudsql"
-CLOUDSQL_USER = "historical-nemos-user"
-CLOUDSQL_PASSWORD = "historical-nemos-user-pass"
 CLOUDSQL_DATABASE = "data"
 CLOUDSQL_CONNECTION_NAME = "trade-278014:southamerica-east1:historical-nemos"
-
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+secrets = secretmanager.SecretManagerServiceClient()
+
+CLOUDSQL_USER = secrets.access_secret_version(
+    "projects/"+PROJECT_ID+"/secrets/trade-web-scraping-db-user/versions/1").payload.data.decode("utf-8")
+CLOUDSQL_PASSWORD = secrets.access_secret_version(
+    "projects/"+PROJECT_ID+"/secrets/trade-web-scraping-db-user-password/versions/1").payload.data.decode("utf-8")
+
+
 # To use cloud proxy to GCP for local development
 LOCAL_SQLALCHEMY_DATABASE_URI = (
     'mysql+pymysql://{user}:{password}@localhost/{database}').format(
